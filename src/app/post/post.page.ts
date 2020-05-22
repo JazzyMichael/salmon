3373256cb1ed1331febc1d06c14298ba7185bb1a
Michael Mancini <thejazzymichael@gmail.com>
2020-05-22 15:20:54 -0400

    post view action sheet and delete confirmation

--- src/app/post/post.page.ts
+++ src/app/post/post.page.ts
@@ -6,6 +6,7 @@ import { SEOService } from '../services/seo.service';
 import { PostService } from '../services/post.service';
 import { AuthService } from '../services/auth.service';
 import { Location } from '@angular/common';
+import { ActionSheetController, AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-post',
@@ -25,7 +26,9 @@ export class PostPage implements OnInit, OnDestroy {
     private seo: SEOService,
     private postService: PostService,
     private auth: AuthService,
-    private location: Location
+    private location: Location,
+    private alertController: AlertController,
+    private actionSheetController: ActionSheetController
   ) { }
 
   ngOnInit() {
@@ -37,7 +40,7 @@ export class PostPage implements OnInit, OnDestroy {
     .subscribe(post => {
       console.log(post);
       this.post = post;
-      this.editable = this.post.userId && this.post.userId === this.auth.uid;
+      this.editable = this.post && this.post.userId && this.post.userId === this.auth.uid;
       this.seo.updateTags({
         title: `${this.post.title} | Salmon`,
         description: `${this.post.description} ${this.post.location}. The Art of Cooking Salmon.`,
@@ -51,8 +54,40 @@ export class PostPage implements OnInit, OnDestroy {
     this.seo.updateTags({});
   }
 
+  share() {
+    console.log('shared!');
+  }
+
   edit() {
-    return;
+    console.log('edited!');
+  }
+
+  delete() {
+    console.log('deleted!');
+  }
+
+  async showDeleteConfirmation() {
+    const alert = await this.alertController.create({
+      header: 'Delete this Post',
+      message: 'Are you sure?',
+      buttons: [
+        { text: 'Cancel', role: 'cancel' },
+        { text: 'Delete', handler: this.delete }
+      ]
+    });
+    await alert.present();
+  }
+
+  async showActionSheet() {
+    const actionSheet = await this.actionSheetController.create({
+      buttons: [
+        { text: 'Delete', role: 'destructive', handler: async () => { await this.showDeleteConfirmation(); return true; } },
+        { text: 'Edit', handler: this.edit },
+        { text: 'Share', handler: this.share },
+        { text: 'Cancel', role: 'cancel' }
+      ]
+    });
+    await actionSheet.present();
   }
 
 }
