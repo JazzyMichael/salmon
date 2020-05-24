91c60d67dfa0f880e91a8195ad24e5e256d4d8a1
Michael Mancini <thejazzymichael@gmail.com>
2020-05-24 08:58:19 -0400

    recent posts on user doc & updated login screen photo

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -38,20 +38,30 @@ export const createUser = functions.auth.user().onCreate(async (user: admin.auth
 
 export const createPost = functions.firestore
     .document('posts/{postId}')
-    .onCreate((snap, context) => {
+    .onCreate(async (snap, context) => {
         const post = snap.data();
-        if (!post || !post.userId) throw new Error('Invalid Post');
-        const increment = admin.firestore.FieldValue.increment(1);
-        // todo: also update recent posts on user doc
-        return db.doc(`users/${post.userId}`).update({ postCount: increment });
+        const postId = context.params.postId;
+        if (!post || !postId || !post.userId) throw new Error('Invalid Post');
+        const postCount = admin.firestore.FieldValue.increment(1);
+
+        const doc = await db.doc(`users/${post.userId}`).get();
+        const user = doc.data() || {};
+        let recentPosts = user.recentPosts || [];
+        const length = recentPosts.unshift({ ...post, id: postId });
+        if (length > 5) {
+            recentPosts = recentPosts.slice(0, 5);
+        }
+
+        return db.doc(`users/${post.userId}`).update({ postCount, recentPosts });
     });
 
 export const deletePost = functions.firestore
     .document('posts/{postId}')
-    .onDelete((snap, context) => {
+    .onDelete(async (snap, context) => {
         const post = snap.data();
+        const postId = context.params.postId;
 
-        if (!post || !post.userId) throw new Error('Invalid Post');
+        if (!post || !post.id || !post.userId) throw new Error('Invalid Post');
 
         const paths = post.images.map((img: any) => img.path);
 
@@ -59,10 +69,18 @@ export const deletePost = functions.firestore
             return admin.storage().bucket('theartofcookingsalmon.appspot.com').file(path).delete();
         });
 
-        const decrement = admin.firestore.FieldValue.increment(-1);
+        const postCount = admin.firestore.FieldValue.increment(-1);
 
-        // todo: also update recent posts on user doc
-        const updateUser = db.doc(`users/${post.userId}`).update({ postCount: decrement });
+        const doc = await db.doc(`users/${post.userId}`).get();
+        const user = doc.data() || {};
+        const recentPosts = user.recentPosts || [];
+        const postIndex = recentPosts.findIndex((p: any) => p.id === postId);
+
+        if (postIndex > -1) {
+            recentPosts.splice(postIndex, 1);
+        }
+
+        const updateUser = db.doc(`users/${post.userId}`).update({ postCount, recentPosts });
 
         promises.push(updateUser);
 
